18921efe4a5e765c984a4ec8e6a546c1fad56864
mango_gills <juzzgil21@gmail.com>
2022-07-13 22:09:29 +0800

    test fix for error upon load

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,7 @@ const app = express();
 
 dbConnection(process.env.MONGODB_URI, "AppDB");
 
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(
   express.urlencoded({
@@ -25,7 +26,6 @@ app.use(
 );
 
 app.use(logger("dev"));
-app.use(cors());
 app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
