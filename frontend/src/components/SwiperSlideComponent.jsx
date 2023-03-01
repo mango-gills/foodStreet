ab789c7b27f5dcb4deeeca57ce324ca5103bd63c
mango_gills <juzzgil21@gmail.com>
2023-03-01 19:49:01 +0800

    scale image on hover, restyle hero description container layout

--- frontend/src/components/SwiperSlideComponent.jsx
+++ frontend/src/components/SwiperSlideComponent.jsx
@@ -3,17 +3,14 @@ import { Link } from "react-router-dom";
 
 const SwiperSlideComponent = ({ post }) => {
   return (
-    <div className="slider-item">
+    <div className="cursor-pointer slider-item">
       <img
-        className="h-[350px] sm:h-[350px] md:h-[400px] lg:h-[550px] w-full"
+        className="h-[350px] sm:h-[350px] md:h-[400px] lg:h-[550px] w-full hover:scale-105 ease-in duration-300"
         src={post.image_url[0].image}
         alt="image"
       />
-      <div className="p-6 xl:py-10 xl:px-20 slider-description">
-        <div
-          className="max-w-xs sm:max-w-sm lg:max-w-[500px] bg-black/90 p-4 md:p-8 rounded-lg
-   flex flex-col justify-between items-start"
-        >
+      <div className="m-8 xl:my-10 xl:mx-16 p-4 lg:p-8 slider-description max-w-xs sm:max-w-sm lg:max-w-[500px] bg-black/90 rounded-lg">
+        <div className="flex flex-col items-start justify-between">
           <h1 className="text-2xl leading-none tracking-wide sm:text-3xl md:text-4xl lg:text-5xl">
             {post.title}
           </h1>
